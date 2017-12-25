16db823ba5c677468cf896a8fc8120d36212d93f
Mathieu <mathieu.0598@gmail.com>
2017-12-25 22:58:05 +0100

    First add

---
+++ cmake-build-debug/CMakeFiles/gtkt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtkt.dir/main.c.obj"
+  "gtkt.pdb"
+  "gtkt.exe"
+  "gtkt.exe.manifest"
+  "libgtkt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gtkt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
